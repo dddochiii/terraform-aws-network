1e18ecaeea2d922013e3c09a568b318ee765637d
권영갑 <lg.yg.kwon@kalmate.net>
2024-11-12 17:42:20 +0900

    create security group for sentinel test (edit module)

--- modules/sg/v0.1/template.tf
+++ modules/sg/v0.1/template.tf
@@ -1,32 +1,34 @@
 ## Security Group ##
 resource "aws_security_group" "sg" {
-    name = join("-",["sg", var.sg_purpose])
+    name = join("-",[var.sg_purpose, "sg"])
     description = join(" ",["Security Group for Access Management about",upper(var.sg_purpose)])
     vpc_id = var.vpc_id
 
     dynamic ingress {
-        count = length(var.ingress_rule)
+        for_each = var.ingress_rule
         
-        protocol = var.ingress_rule[count.index][0]
-        from_port = var.ingress_rule[count.index][1]
-        to_port = var.ingress_rule[count.index][1]
-        cidr_blocks = [regex("^(([1-9]?\\d|1\\d{2}|2([0-4]\\d)|25[0-5])\\.){3}([1-9]?\\d|1\\d{2}|2([0-4]\\d)|25[0-5])$", var.ingress_rule[count.index][2])]
-        security_groups = [regex("^sg-[1-9a-z]{17}$", var.ingress_rule[count.index][2])]
-        description = var.ingress_rule[count.index][3]
+        content {
+            description = ingress.value[3]
+            from_port = ingress.value[1]
+            to_port = ingress.value[1]
+            protocol = ingress.value[0]
+            cidr_blocks = [ingress.value[2]]
+        }        
     }
 
     dynamic egress {
-        count = length(var.egress_rule)
+        for_each = var.egress_rule
         
-        protocol = var.ingress_rule[count.index][0]
-        from_port = var.ingress_rule[count.index][1]
-        to_port = var.ingress_rule[count.index][1]
-        cidr_blocks = [regex("^(([1-9]?\\d|1\\d{2}|2([0-4]\\d)|25[0-5])\\.){3}([1-9]?\\d|1\\d{2}|2([0-4]\\d)|25[0-5])$", var.ingress_rule[count.index][2])]
-        security_groups = [regex("^sg-[1-9a-z]{17}$", var.ingress_rule[count.index][2])]
-        description = var.ingress_rule[count.index][3]
+        content {
+            description = egress.value[3]
+            from_port = egress.value[1]
+            to_port = egress.value[1]
+            protocol = egress.value[0]
+            cidr_blocks = [egress.value[2]]
+        }        
     }
 
     tags = {
-        Name = join("-",["sg", var.sg_purpose])
+        Name = join("-",[var.sg_purpose, "sg"])
     }
 }
